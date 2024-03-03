37fc1081a9c5e25406d92cc5ab16110bb6c72286
guiguirg <guirodlog@gmail.com>
2024-03-03 13:57:49 -0300

    asas
    asasas

--- git_teste_geral/Assets/Scripts/TestScript.cs
+++ git_teste_geral/Assets/Scripts/TestScript.cs
@@ -1,12 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestScript : MonoBehaviour
 {
+    public Text dinheiro;
+
+    public int dinheirou;
+
+
+    private void Awake()
+    {
+        dinheirou = 1000;
+    }
+
     // Update is called once per frame
     void Update()
     {
+
+        dinheiro.text = "Dinheiro: " + dinheirou;
+
         if (Input.GetKeyDown(KeyCode.T))
         {
             Debug.Log("Testing Successful!");
